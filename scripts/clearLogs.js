c67cb1fe9df2cf0ab56a333c79e4bf2dfff48028
Anju <anjumpkr4115@gmail.com>
2026-01-15 09:14:00 +0530

    log clearence

---
+++ scripts/clearLogs.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const logFiles = [
+  path.join(__dirname, '../logs/combined.log'),
+  path.join(__dirname, '../logs/error.log'),
+];
+
+logFiles.forEach((file) => {
+  if (fs.existsSync(file)) {
+    fs.truncateSync(file, 0); 
+    console.log(`Cleared: ${file}`);
+  }
+});
