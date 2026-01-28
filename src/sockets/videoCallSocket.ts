5170c0ad1d310002558f34125b5939a82f8cb5d4
Anju <anjumpkr4115@gmail.com>
2026-01-28 16:34:05 +0530

    videocall socket changes

--- src/sockets/videoCallSocket.ts
+++ src/sockets/videoCallSocket.ts
@@ -1,39 +1,38 @@
-// import { Server, Socket } from "socket.io";
+
+
+// import { Server, Socket } from 'socket.io';
 
 // export default function videoCallSocket(io: Server) {
-//   io.on("connection", (socket: Socket) => {
-//     console.log("Video socket connected:", socket.id);
+//   io.on('connection', (socket: Socket) => {
+//     console.log('Video socket connected:', socket.id);
 
-//     socket.on("join-room", (roomId: string) => {
+//     socket.on('join-room', (roomId: string) => {
 //       socket.join(roomId);
-//       console.log(`Socket ${socket.id} joined room ${roomId}`);
+//       socket.to(roomId).emit('user-joined');
 //     });
 
-//     socket.on("offer", ({ roomId, offer }) => {
-//       socket.to(roomId).emit("offer", offer);
+//     socket.on('offer', ({ roomId, offer }) => {
+//       socket.to(roomId).emit('offer', offer);
 //     });
 
-//     socket.on("answer", ({ roomId, answer }) => {
-//       socket.to(roomId).emit("answer", answer);
+//     socket.on('answer', ({ roomId, answer }) => {
+//       socket.to(roomId).emit('answer', answer);
 //     });
 
-//     socket.on("ice-candidate", ({ roomId, candidate }) => {
-//       socket.to(roomId).emit("ice-candidate", candidate);
+//     socket.on('ice-candidate', ({ roomId, candidate }) => {
+//       socket.to(roomId).emit('ice-candidate', candidate);
 //     });
 
-//     socket.on("leave-room", (roomId: string) => {
-//       socket.leave(roomId);
+//     socket.on('end-call', (roomId: string) => {
+//       socket.to(roomId).emit('end-call');
 //     });
 
-//     socket.on("end-call", roomId => {
-//         socket.to(roomId).emit("end-call");
-//     });
-
-//     socket.on("disconnect", () => {
-//       console.log("Video socket disconnected:", socket.id);
+//     socket.on('disconnect', () => {
+//       console.log('Video socket disconnected:', socket.id);
 //     });
 //   });
 // }
+
 
 import { Server, Socket } from 'socket.io';
 
@@ -46,17 +45,29 @@ export default function videoCallSocket(io: Server) {
       socket.to(roomId).emit('user-joined');
     });
 
+    // socket.on('offer', ({ roomId, offer }) => {
+    //   socket.to(roomId).emit('offer', offer);
+    // });
+
+    // socket.on('answer', ({ roomId, answer }) => {
+    //   socket.to(roomId).emit('answer', answer);
+    // });
+
+    // socket.on('ice-candidate', ({ roomId, candidate }) => {
+    //   socket.to(roomId).emit('ice-candidate', candidate);
+    // });
+
     socket.on('offer', ({ roomId, offer }) => {
-      socket.to(roomId).emit('offer', offer);
-    });
+  socket.to(roomId).emit('offer', { offer });
+});
 
-    socket.on('answer', ({ roomId, answer }) => {
-      socket.to(roomId).emit('answer', answer);
-    });
+socket.on('answer', ({ roomId, answer }) => {
+  socket.to(roomId).emit('answer', { answer });
+});
 
-    socket.on('ice-candidate', ({ roomId, candidate }) => {
-      socket.to(roomId).emit('ice-candidate', candidate);
-    });
+socket.on('ice-candidate', ({ roomId, candidate }) => {
+  socket.to(roomId).emit('ice-candidate', { candidate });
+});
 
     socket.on('end-call', (roomId: string) => {
       socket.to(roomId).emit('end-call');
